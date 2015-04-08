55b1a2458e66f15dacca7a92c62f4834d6236401
jacob-wj <wangjia199055@126.com>
2015-04-08 11:51:35 +0800

    完成具有环形效果的list

--- app/src/main/java/com/jacob/circle/list/ListItemView.java
+++ app/src/main/java/com/jacob/circle/list/ListItemView.java
@@ -4,8 +4,6 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.util.AttributeSet;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -16,11 +14,26 @@ import android.widget.TextView;
 public class ListItemView extends LinearLayout {
     private ImageView mImageView;
     private TextView mTextView;
-    private int mParentHeight;
+    /**
+     * listView的一半的高度，这个要根据高度作为分割线
+     */
     private int mHalfHeight;
+
+    /**
+     * 最大的旋转角度
+     */
     private float fullAngelFactor = 20f;
+    /**
+     * 最大的缩放
+     */
     private float fullScaleFactor = 1;
+    /**
+     * X轴最大的偏移量
+     */
     private float fullTranslateXFactor = 40;
+    /**
+     * Y轴最大的偏移量
+     */
     private float fullTranslateYFactor = 35;
 
     public ListItemView(Context context) {
@@ -33,7 +46,7 @@ public class ListItemView extends LinearLayout {
 
     public ListItemView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        LayoutInflater.from(context).inflate(R.layout.layout_list_item, this);
+//        LayoutInflater.from(context).inflate(R.layout.layout_list_item, this);
         mImageView = (ImageView) findViewById(R.id.image_view);
         mTextView = (TextView) findViewById(R.id.text_view);
     }
@@ -45,7 +58,7 @@ public class ListItemView extends LinearLayout {
         int top = getTop();
         float rotate = calculateAngel(top);
         float scale = calculateScale(top);
-        float translateX = calculateTranslate(top);
+        float translateX = calculateTranslateX(top);
         float translateY = (1-scale)*fullTranslateYFactor;
 
         Matrix matrix = canvas.getMatrix();
@@ -58,15 +71,22 @@ public class ListItemView extends LinearLayout {
     }
 
     public void setAppBean(AppBean appBean) {
+        mImageView = (ImageView) findViewById(R.id.image_view);
+        mTextView = (TextView) findViewById(R.id.text_view);
         mImageView.setImageResource(appBean.getAvatar());
         mTextView.setText(appBean.getName());
     }
 
+    /**
+     * 传入listView的高度，并且计算一半的高度
+     */
     public void setParentHeight(int height) {
-        mParentHeight = height;
         mHalfHeight = (int) (height / 2f);
     }
 
+    /**
+     * 计算角度，根据top和halfHeight的比值
+     */
     private float calculateAngel(int top) {
         float result = 0f;
         if (top < mHalfHeight) {
@@ -74,17 +94,22 @@ public class ListItemView extends LinearLayout {
         } else if (top > mHalfHeight) {
             result = (top - mHalfHeight) * 1.0f / mHalfHeight * fullAngelFactor;
         }
-        Log.e("TAG", result + "");
         return result;
     }
 
+    /**
+     * 计算缩放的比例
+     */
     private float calculateScale(int top) {
         float result = 0f;
         result = (1f - 1f / 2f * Math.abs((top - mHalfHeight)) / mHalfHeight) * fullScaleFactor;
         return result;
     }
 
-    private float calculateTranslate(int top) {
+    /**
+     * 计算X偏移的位置
+     */
+    private float calculateTranslateX(int top) {
         float result = 0f;
         result = (1f - 1f / 2f * Math.abs((top - mHalfHeight)) / mHalfHeight) * fullTranslateXFactor;
         return result;
--- app/src/main/java/com/jacob/circle/list/MainActivity.java
+++ app/src/main/java/com/jacob/circle/list/MainActivity.java
@@ -3,10 +3,13 @@ package com.jacob.circle.list;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
@@ -22,12 +25,14 @@ public class MainActivity extends FragmentActivity {
     private ListView mListView;
     private List<AppBean> mAppBeanList = new ArrayList<>();
 
+    private int mItemHeight;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initData();
-
+        mItemHeight = getScreenHeight()/5;
         mListView = (ListView) findViewById(R.id.listView);
         MyListAdapter adapter = new MyListAdapter();
         mListView.setAdapter(adapter);
@@ -91,7 +96,7 @@ public class MainActivity extends FragmentActivity {
         public View getView(int position, View convertView, ViewGroup parent) {
             ListItemView listItemView;
             if (convertView == null) {
-                listItemView = new ListItemView(getApplication());
+                listItemView  = (ListItemView) LayoutInflater.from(getApplication()).inflate(R.layout.layout_list_item,parent,false);
                 convertView = listItemView;
             } else {
                 listItemView = (ListItemView) convertView;
@@ -103,4 +108,11 @@ public class MainActivity extends FragmentActivity {
         }
     }
 
+
+    private int getScreenHeight(){
+        WindowManager windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        DisplayMetrics metrics = new DisplayMetrics();
+        windowManager.getDefaultDisplay().getMetrics(metrics);
+        return metrics.widthPixels;
+    }
 }
